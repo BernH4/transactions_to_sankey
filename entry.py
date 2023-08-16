0d3e42c5c8dc898e72e1802379124bd19e622253
Bernhard Steidl <bernhard.s.dev@gmail.com>
2023-08-16 21:53:23 +0200

    New Feature AND and OR condition in rules

--- entry.py
+++ entry.py
@@ -8,42 +8,48 @@ class Entry:
     def __init__(self, line, config):
         self.line = line
         #self.verwendungszweck = entry['']
-        self.group, self.subgroup = self.associate_to_group(config)
+        self.category, self.subcategory = self.associate_to_category(config)
 
     def printc(self, color, string):
         print(f"\n{color}{string}{Style.RESET_ALL}")
         
-    def associate_to_group(self, config):
-        group_matches = 0
+    def associate_to_category(self, config):
+        category_matches = 0
         #Sollte nur eine Gruppe sein, falls doch mehr gematched werden wird das Ausgegeben, Array benötigt
-        groups = []
-        subgroups = []
+        categories = []
+        subcategories = []
         for  typ in config['typ']:
+            matches_for_curr_category = []
             for  spalte, regex in typ['suche']:
-                try:
-                    if re.search(regex, self.line[spalte], flags=re.IGNORECASE):
-                        group_matches += 1
-                        groups.append(typ['group'])
-                        subgroups.append(typ['subgroup'])
-                        break #Man kann für einen Typ mehrere Regexe angeben, matched einer ist es nicht nötig weitere auszuprobieren. Es werden aber weiterhin andere Typen ausprobiert ob diese auch matchen,  falls ja muss man eventuell die Regeln anpassen
-                except TypeError:
-                    # Keine IBAN in Buchung angegeben, wahrscheinlich weil Auszahlung Geldautomat
-                    if spalte == 'IBAN Zahlungsbeteiligter' and pd.isnull(self.line[spalte]):
-                        continue
+                content = self.line[spalte]
+                # Check if content is NaN or None and assign empty string, else re.search will throw an error
+                content = "" if  pd.isnull(content) else content
+                matches_for_curr_category.append(re.search(regex, content, flags=re.IGNORECASE))
+            
+            if typ.get('match_type') == None or typ['match_type'] == "OR":
+                if any(matches_for_curr_category):
+                    category_matches += 1
+                    categories.append(typ['category'])
+                    subcategories.append(typ['subcategory'])
+            elif typ['match_type'] == "AND":
+                if all(matches_for_curr_category):
+                    category_matches += 1
+                    categories.append(typ['category'])
+                    subcategories.append(typ['subcategory'])
 
-        if group_matches > 1:
+        if category_matches > 1:
             #Hier debugger einschalten und gruppe multiple matches checken
             self.printc(Fore.RED, "Es wurden mehrere matches gefunden für folgende Zeile, eventuel Regeln anpassen:")
             print(self.line[['Name Zahlungsbeteiligter', 'Betrag','Buchungstag', 'Verwendungszweck']].to_string())
-            print("Groups: ", groups[0], "\n", subgroups[0])
+            print("Groups: ", categories[0], "\n", subcategories[0])
             return (None, None)
-        elif group_matches == 0:
+        elif category_matches == 0:
             self.printc(Fore.RED, "Folgende Zeile konnte nicht zugeordnet werden:")
             print(self.line[['Name Zahlungsbeteiligter', 'Betrag','Buchungstag', 'Verwendungszweck']].to_string())
             return (None, None)
         else:
             self.printc(Fore.GREEN, "OK:")
             print(self.line[['Name Zahlungsbeteiligter', 'Betrag','Buchungstag', 'Verwendungszweck']].to_string())
-            print("Groups: ", groups[0], "\nSubgroup: ", subgroups[0])
-            return (groups[0], subgroups[0])
+            print("Groups: ", categories[0], "\nSubcategory: ", subcategories[0])
+            return (categories[0], subcategories[0])
 
